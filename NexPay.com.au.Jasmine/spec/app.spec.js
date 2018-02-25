8250d2de8c6011a97839db9d2f50d73752afbec8
john.hadikusumo@gmail.com <john.hadikusumo@gmail.com>
2018-02-25 21:04:04 +1100

    update

---
+++ NexPay.com.au.Jasmine/spec/app.spec.js
@@ -0,0 +1,26 @@
+﻿describe('app', function () {
+    var input = {
+        BSBNo: '420377',
+        AccountNO: '90031551',
+        AccountName: 'John Doe',
+        Reference: 'Billing Payment',
+        Amount: 425.25
+    };
+
+    var output = { "status": true }
+
+    var dataService = {};
+
+    beforeEach(module('myApp'));
+    beforeEach(module('ngRoute'));
+    beforeEach(module('ui.bootstrap'));
+
+
+    beforeEach(angular.mock.inject(function (_dataService_) {
+        dataService = _dataService_;
+    }));
+
+    it('Save Bank Payment', function () {
+        expect(dataService.save(input)).toEqual(output);
+    });
+});
